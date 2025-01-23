b08648d0390a3a34370b6404dd2292b2668ae648
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-01-23 12:45:31 +0100

    remove button to show more, add padding for description

--- build/js/projects.js
+++ build/js/projects.js
@@ -31,34 +31,18 @@ export function projects() {
           : "<span>No GitHub repo available.</span>";
 
         projectDiv.innerHTML = `
-          <img src="${project.image}" alt="${project.title}" class="w-full h-auto">
-          <h3 class="font-bold text-center pt-5 text-2xl">${project.title}</h3>
-          <div class="py-5">
-            <p class="description hidden">${project.description}</p>
-            <button class="toggle-description mx-5 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700">
-              Show More
-            </button>
-          </div>
-          <div>
-          <p class="text-center"><strong>Tools:</strong> ${project.tools.join(", ")}.</p>
-          </div>
-          <p class="text-center pb-5">${link} | ${githubLink}</p>
-        `;
+            <img src="${project.image}" alt="${project.title}" class="w-full h-auto">
+            <h3 class="font-bold text-center pt-5 text-2xl">${project.title}</h3>
+            <div class="py-5">
+              <p class="description pl-5 pr-5">${project.description}</p>
+            </div>
+            <div>
+            <p class="text-center"><strong>Tools:</strong> ${project.tools.join(", ")}.</p>
+            </div>
+            <p class="text-center pb-5">${link} | ${githubLink}</p>
+          `;
 
         container.appendChild(projectDiv);
-
-        const toggleButton = projectDiv.querySelector(".toggle-description");
-        const description = projectDiv.querySelector(".description");
-
-        toggleButton.addEventListener("click", () => {
-          if (description.classList.contains("hidden")) {
-            description.classList.remove("hidden");
-            toggleButton.textContent = "Show Less";
-          } else {
-            description.classList.add("hidden");
-            toggleButton.textContent = "Show More";
-          }
-        });
       });
     })
     .catch((error) => console.error("Error fetching JSON:", error));
