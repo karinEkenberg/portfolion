96a59767ca337c600405607b9580c38b87f7102e
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-01-28 14:05:08 +0100

    add aboout

---
+++ build/js/about.js
@@ -0,0 +1,11 @@
+export async function LoadAbout() {
+  try {
+    const response = await fetch("about.html");
+    if (!response.ok) throw new Error("Could not fetch about.html");
+
+    const htmlContent = await response.text();
+    document.getElementById("about-placeholder").innerHTML = htmlContent;
+  } catch (error) {
+    console.error("Error loading sills:", error);
+  }
+}
--- build/js/script.js
+++ build/js/script.js
@@ -1,10 +1,12 @@
 import { Header } from "./header.js";
 import { projects } from "./projects.js";
 import { Skills, LoadSkills } from "./skills.js";
+import { LoadAbout } from "./about.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   Header();
   projects();
   Skills();
   LoadSkills();
+  LoadAbout();
 });
